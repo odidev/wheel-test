4cc480ba860f500e113db6d00b62350cd096b5e2
Janakarajan Natarajan <janakan@amazon.com>
2020-07-20 21:44:33 +0000

    Push directory fix for travis-ci

--- build-wheels.sh
+++ build-wheels.sh
@@ -2,11 +2,12 @@
 set -e -u -x
 
 cd /io/
-rm -f /io/wheelhouse/*
+rm -rf /io/wheelhouse
+mkdir /io/wheelhouse
 yum update -y
 /opt/python/cp39-cp39/bin/python3.9 /io/setup.py bdist_wheel
-cp /io/dist/*.whl /io/wheelhouse/
-for whl in wheelhouse/*_aarch64.whl; do
-	auditwheel repair $whl --plat "manylinux2014_aarch64" -w /io/wheelhouse/
+cp /io/dist/*.whl /io/
+for whl in /io/*_aarch64.whl; do
+	auditwheel repair $whl --plat "manylinux2014_aarch64" -w /io/
 done
-/opt/python/cp39-cp39/bin/pip3.9 install /io/wheelhouse/*-manylinux2014_aarch64.whl
+/opt/python/cp39-cp39/bin/pip3.9 install /io/*-manylinux2014_aarch64.whl
