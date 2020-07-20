d2b649a161d9276c30a46214c6f6f1c3e81181a0
Janakarajan Natarajan <janakan@amazon.com>
2020-07-20 21:35:52 +0000

    Fix for build failure

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e -u -x
 
+cd /io/
+rm -f /io/wheelhouse/*
 yum update -y
 /opt/python/cp39-cp39/bin/python3.9 /io/setup.py bdist_wheel
 cp /io/dist/*.whl /io/wheelhouse/
 for whl in wheelhouse/*_aarch64.whl; do
 	auditwheel repair $whl --plat "manylinux2014_aarch64" -w /io/wheelhouse/
 done
-cd wheelhouse/
+/opt/python/cp39-cp39/bin/pip3.9 install /io/wheelhouse/*-manylinux2014_aarch64.whl
--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 
 if __name__ == '__main__':
 
-    from setuptools import setup, Extension, Distribution
+    from setuptools import setup, Extension
+
+    extension_module = Extension(
+            'fputs',
+            sources=['fputsmodule.c'],
+    )
 
     setup(
-            name="fputs",
+            name="wheel_test",
             version="1.0.0",
             description="Python interface for the fputs C library function",
             author="Janakarajan Natarajan",
             author_email="janakarajan.natarajan@example.com",
-            ext_modules=[Extension("fputs", ["fputsmodule.c"])],
+            ext_modules=[extension_module],
     )
